84f4299c5cc6880b301124631e93ad4b9f1f18cc
samydilini <samuditha.s@gmail.com>
2019-03-11 19:05:39 +1100

    Testes Match for Three and one points
    Change-Id: I3b729616bd543c56f0d993a8aefc6786a50f93f2

--- com.dius.tennis/src/test/java/com/dius/com/dius/tennis/functionality/MatchTest.java
+++ com.dius.tennis/src/test/java/com/dius/com/dius/tennis/functionality/MatchTest.java
@@ -1,6 +1,6 @@
 package com.dius.com.dius.tennis.functionality;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
 
 import org.junit.Test;
 
@@ -15,4 +15,15 @@ public class MatchTest {
 
 	}
 
+	@Test
+	public void testThreeOne() throws Exception {
+		Match match = new Match("player 1", "player 2");
+		match.pointWonBy("player 1");
+		match.pointWonBy("player 2");
+
+		match.pointWonBy("player 1");
+		match.pointWonBy("player 1");
+		// this will return "0-0, 40-15"
+		assertEquals("0-0, 40-15", match.score());
+	}
 }
