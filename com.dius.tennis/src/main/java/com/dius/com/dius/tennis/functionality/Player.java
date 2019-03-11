70b26235a5fbf9d76f00b04b29b2d893d17755c9
samydilini <samuditha.s@gmail.com>
2019-03-11 19:00:51 +1100

    Added code for player test Completed code for testNormalScoreZeroZero in GameTest
    Change-Id: I5ca63db25c5a7fb46871612594fc41e671510af5

--- com.dius.tennis/src/main/java/com/dius/com/dius/tennis/functionality/Player.java
+++ com.dius.tennis/src/main/java/com/dius/com/dius/tennis/functionality/Player.java
@@ -2,14 +2,17 @@ package com.dius.com.dius.tennis.functionality;
 
 public class Player {
 	private int point;
+	private int gameCount;
 
 	public void addPoint() {
 		point++;
-
 	}
 
 	public int getPoint() {
 		return point;
 	}
 
+	public int getGameCount() {
+		return gameCount;
+	}
 }
