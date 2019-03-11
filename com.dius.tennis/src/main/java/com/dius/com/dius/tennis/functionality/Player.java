f670934738f582472b83c2e9d27bdc6293cbe3cc
samydilini <samuditha.s@gmail.com>
2019-03-11 18:49:56 +1100

    Added coding for add point
    Change-Id: I7cf18223e41094a39a3be43824ef463abb3c3ae6

--- com.dius.tennis/src/main/java/com/dius/com/dius/tennis/functionality/Player.java
+++ com.dius.tennis/src/main/java/com/dius/com/dius/tennis/functionality/Player.java
@@ -1,5 +1,15 @@
 package com.dius.com.dius.tennis.functionality;
 
 public class Player {
+	private int point;
+
+	public void addPoint() {
+		point++;
+
+	}
+
+	public int getPoint() {
+		return point;
+	}
 
 }
